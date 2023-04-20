1d50b5c3eb6d9c9df677ad088ff9d0619c48b266
OleksiiProkoshin <my.mail2210@gmail.com>
2023-04-20 19:46:40 +0300

    mobile

--- swiper.js
+++ swiper.js
@@ -1,22 +1,17 @@
 const swiper = new Swiper(".watch-swiper", {
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
   slidesPerView: 1,
   spaceBetween: 5,
+
+  speed: 800,
   
     keyboard: {
     enabled: true,
     onlyInViewport: true,
     pageUpDown: true,
   },
-  mouseweel: {
-    sensitivity: 1,
-    eventsTarget: ".swiper-slide",
-  },
-  freeMode: true,
-  autoplay: {
-    delay: 1000,
-    stopOnLastSlide: false,
-    disableOnInteraction: true,
-    loop: true,
-    speed: 600,
-  }
+    
    });
